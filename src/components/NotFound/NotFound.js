9c83afe58e5e1485c345bb8c4b4ef2e566ec8d56
Daria Takush <dashatakush@yandex.ru>
2021-09-15 14:41:28 +0300

    level-4/burFix

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -10,10 +10,11 @@ function NotFound(props) {
                 <h1 className="not-found__title">404</h1>
                 <p className="not-found__subtitle">Страница не найдена</p>
             </div>
+
             <button
                 className="not-found__button"
                 type="button"
-                onClick={() => history.goBack()}
+                onClick={history.goBack}
             >
                 Назад
             </button>
